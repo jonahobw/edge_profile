629540cfbfac93f39cbf76cc50203de3daf3dc9b
jonahobw <jgow98@gmail.com>
2022-03-04 13:00:49 -0500

    smooth output

--- exec.py
+++ exec.py
@@ -6,6 +6,5 @@ if len(args) == 0:
     raise ValueError("Must provide a path to an executable file.")
 print(f"Running {args[0]} with arguments {args[1:]}")
 
-output = subprocess.run(args, capture_output=True)
-print(output.stdout)
+output = subprocess.run(args, stdout=sys.stdout)
 
